1f5c5e5fb55f27b51e81a208b2dd17b49f868946
hunor64 <nhunor06@gmail.com>
2024-09-16 11:46:02 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Pizza_Site
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
