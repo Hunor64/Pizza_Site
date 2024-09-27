95b2c7711bdca338fbe1e88dec4269ba5efe0bac
marcellnagy05 <marcika863@gmail.com>
2024-09-27 14:02:28 +0200

    Admin panel and edit db needs debug

--- Admin_Panel.xaml.cs
+++ Admin_Panel.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Pizza_Site.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Pizza_Site.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +21,36 @@ namespace Pizza_Site
 {
     public partial class Admin_Panel : Window
     {
-        
+        public ObservableCollection<Pizza> Pizzas { get; set; }
         public Admin_Panel()
         {
             InitializeComponent();
+            LoadPizzasFromDatabase();
+            DataContext = this;
         }
+
+        private void LoadPizzasFromDatabase()
+        {
+            using (var newContext = new PizzaContext())
+            {
+                //Creating an itemsource list to our grid and pushing the datas from the database to it
+                List<Pizza> pizzaList = new List<Pizza>();
+
+                //Creating a list from our PizzaDescription table
+                var pizzas = newContext.PizzasDescription.ToList();
+
+                //Giving values to the elements to our list 
+                foreach (var pizza in pizzas)
+                {
+                    pizzaList.Add(new Pizza { Name = pizza.PizzaName, ImagePath = $"pack://application:,,,/Pizza_Site;component/Images/{pizza.ImagePath.ToLower()}", Ingredients = pizza.Ingredients, Price = pizza.Price }); ;
+                }
+
+
+                //Setting the main grid's itemsource to our appended list
+                PizzaListView.ItemsSource = pizzaList;
+            }
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -31,7 +60,7 @@ namespace Pizza_Site
         #region Custom title bar clicks
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            DragMove();
+            //DragMove();
         }
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
@@ -69,7 +98,7 @@ namespace Pizza_Site
                 //Giving values to the elements to our list 
                 foreach (var pizza in pizzas)
                 {
-                    pizzaList.Add(new Pizza { Name = pizza.PizzaName, ImagePath = $"pack://application:,,,/Pizza_Site;component/Images/{pizza.ImagePath.ToLower()}", Ingredients = pizza.Ingredients, Price = pizza.Price }); ;
+                    pizzaList.Add(new Pizza { Name = pizza.PizzaName, ImagePath = $"pack://application:,,,/Pizza_Site;component/Images/{pizza.ImagePath.ToLower()}", Ingredients = pizza.Ingredients, Price = pizza.Price });
                 }
 
 
@@ -77,6 +106,24 @@ namespace Pizza_Site
                 PizzaListView.ItemsSource = pizzaList;
                 
             }
+        }
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+        public void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //using (var context = new PizzaContext())
+            //{
+            //    //Passing data to a new user
+            //    var newUser = new PizzaUsers { User_Name = userName, User_Password = userPassword, User_Email = userEmail, User_MobileNumber = userMobileNumber, User_Address = userAddress, Is_Admin = is_Admin };
+
+            //    //Adding new user to our list
+            //    context.PizzaStore.Add(newUser);
+
+            //    //Saving the changes
+            //    context.SaveChanges();
+            //}
         }
     }
 }
---
+++ EditDb.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pizza_Site
+{
+    /// <summary>
+    /// Interaction logic for EditDb.xaml
+    /// </summary>
+    public partial class EditDb : Window
+    {
+        public EditDb()
+        {
+            InitializeComponent();
+        }
+        #region Custom title bar clicks
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                Register.GetWindow(this).Width = 350;
+                Register.GetWindow(this).Height = 450;
+            }
+            else if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+                Register.GetWindow(this).Width = System.Windows.SystemParameters.WorkArea.Width;
+                Register.GetWindow(this).Height = System.Windows.SystemParameters.WorkArea.Height;
+            };
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        private void SavePizza_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
